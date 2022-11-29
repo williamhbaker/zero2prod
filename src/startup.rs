e8639b17273984a893d4287033e15672d368caeb
Will Baker <wbaker@gmail.com>
2022-11-29 18:44:02 -0500

    fix variable names

--- src/startup.rs
+++ src/startup.rs
@@ -4,14 +4,14 @@ use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 
-pub fn run(listener: TcpListener, db: PgPool) -> Result<Server, std::io::Error> {
-    let db = web::Data::new(db);
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
 
     Ok(HttpServer::new(move || {
         App::new()
             .route("/health", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(db.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run())
